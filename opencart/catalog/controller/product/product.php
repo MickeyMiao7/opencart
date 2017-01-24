bc43c857932b3b2271ce28d1c3adb08ffa2a55c2
Mickey <327191467@qq.com>
2017-01-24 17:04:54 +0800

    toy

--- opencart/catalog/controller/product/product.php
+++ opencart/catalog/controller/product/product.php
@@ -469,7 +469,7 @@ class ControllerProductProduct extends Controller {
 
 			// Get recommendation info for the product
 			$categories = $this->model_catalog_product->getCategories($product_id);
-			$this->data['tmp'] = $categories;
+//			$this->data['tmp'] = $categories;
 			//			foreach ($categories as $category){
 //                $data = array(
 //                    'filter_category_id' => $category['category_id']
