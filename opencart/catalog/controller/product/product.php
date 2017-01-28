1d15c3e5df10b40465f5f4a288dca9aa1f8435fc
Mickey <327191467@qq.com>
2017-01-28 14:21:30 +0800

    toy

--- opencart/catalog/controller/product/product.php
+++ opencart/catalog/controller/product/product.php
@@ -512,8 +512,8 @@ class ControllerProductProduct extends Controller {
                         'price'       => $price,
                         'special'     => $special,
                         'tax'         => $tax,
-                        'rating'      => $product['rating']
-//                        'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $best_seller_product['product_id'] . $url)
+                        'rating'      => $product['rating'],
+                        'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $best_seller_product['product_id'] . $url)
                     );
 
                 }
