30d082e1ca1e19700e98984fcbe2f2a405e429c0
Mickey <327191467@qq.com>
2017-02-01 16:57:01 +0800

    optimize recommendation

--- opencart/catalog/controller/product/product.php
+++ opencart/catalog/controller/product/product.php
@@ -473,11 +473,12 @@ class ControllerProductProduct extends Controller {
             $this->data['best_seller_products'] = array();
 
             foreach ($categories as $category) {
+            	$count = 1;
                 $data = array('filter_category_id' => $category['category_id']);
                 $products = $this->model_catalog_product->getProducts($data);
-                $best_seller_products_temp = $this->model_catalog_product->getBestSellerProductsByCategory($category['category_id'], 3);
+                $best_seller_products_temp = $this->model_catalog_product->getBestSellerProductsByCategory($category['category_id'], 6);
                 foreach ($best_seller_products_temp as $product) {
-                	if (in_array($product['product_id'],$this->data['best_seller_products_ids']) || $product['product_id'] == $product_id) {
+                	if (in_array($product['product_id'], $this->data['best_seller_products_ids']) || $product['product_id'] == $product_id) {
                         continue;
                     }
                     $product_ids[] = $product['product_id'];
@@ -516,10 +517,16 @@ class ControllerProductProduct extends Controller {
                         'special' => $special,
                         'tax' => $tax,
                         'rating' => $rating,
-//                        'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $product['product_id'] . $url)
                         'href' => $this->url->link('product/product' . '&product_id=' . $product['product_id'] . $url)
                     );
-					break;
+                    $count += 1;
+					if ((count($categories) == 3 ) && $count == 2)
+						break;
+                    if ((count($categories) == 2 ) && $count == 3)
+                        break;
+                    if ((count($categories) == 2 ) && $count == 6)
+                        break;
+
                 }
             }
 
