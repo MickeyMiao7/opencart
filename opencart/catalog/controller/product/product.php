c7a52d5457b5fb416cd4fac40a593959de28e5d9
Mickey <327191467@qq.com>
2017-02-01 17:29:04 +0800

    optimize recommendation

--- opencart/catalog/controller/product/product.php
+++ opencart/catalog/controller/product/product.php
@@ -478,7 +478,7 @@ class ControllerProductProduct extends Controller {
             	$count = 0;
                 $data = array('filter_category_id' => $category['category_id']);
                 $products = $this->model_catalog_product->getProducts($data);
-                $best_seller_products_temp = $this->model_catalog_product->getBestSellerProductsByCategory($category['category_id'], 5);
+                $best_seller_products_temp = $this->model_catalog_product->getBestSellerProductsByCategory($category['category_id'], 6);
                 foreach ($best_seller_products_temp as $product) {
                 	if (in_array($product['product_id'], $this->data['best_seller_products_ids']) || $product['product_id'] == $product_id) {
                         continue;
@@ -523,13 +523,13 @@ class ControllerProductProduct extends Controller {
                     );
                     $count += 1;
 					if ((count($categories) == 3 ) && $count == 2)
-						break;
-                    if ((count($categories) == 3 ) && $turn == 3 && $count == 1)
-                        break;
-                    if ((count($categories) == 2 ) && $count == 2)
-                        break;
-                    if ((count($categories) == 1 ) && $count == 4)
-                        break;
+					{break;}
+                    if ((count($categories) == 3 ) && $turn == 3 && $count == 1){
+                        break;}
+                    if ((count($categories) == 2 ) && $count == 2){
+                        break;}
+                    if ((count($categories) == 1 ) && $count == 4){
+                        break;}
 
                 }
             }
