bed6c21e6b19de9ffeb7e22c8094d0d5fb61c4a5
Mickey <327191467@qq.com>
2017-02-01 22:18:18 +0800

    optimize recommendation

--- opencart/catalog/controller/product/product.php
+++ opencart/catalog/controller/product/product.php
@@ -523,7 +523,7 @@ class ControllerProductProduct extends Controller {
                     );
                 }
             }
-            $this->data['products_under_same_category'] = array_unique($this->data['products_under_same_category']);
+//            $this->data['products_under_same_category'] = array_unique($this->data['products_under_same_category']);
             $this->data['best_seller_products_ids'] = array();
             $best_seller_products = $this->model_catalog_product->getBestSellerProducts(100);
 //            $count = 0;
