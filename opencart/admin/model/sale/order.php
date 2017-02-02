d3aba1b9efb8800e972480de7d70d427abe81cf2
Mickey <327191467@qq.com>
2017-02-02 16:07:16 +0800

    debug export xls

--- opencart/admin/model/sale/order.php
+++ opencart/admin/model/sale/order.php
@@ -493,7 +493,8 @@ class ModelSaleOrder extends Model {
 				'user_agent'              => $order_query->row['user_agent'],	
 				'accept_language'         => $order_query->row['accept_language'],					
 				'date_added'              => $order_query->row['date_added'],
-				'date_modified'           => $order_query->row['date_modified']
+				'date_modified'           => $order_query->row['date_modified'],
+                'referrer_id'             => $order_query->row['referrer_id']
 			);
 		} else {
 			return false;
