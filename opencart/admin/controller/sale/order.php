bcec3285a66c4f503c9b3bf487bc954e85c603b5
Mickey <327191467@qq.com>
2017-02-02 16:30:05 +0800

    debug export xls

--- opencart/admin/controller/sale/order.php
+++ opencart/admin/controller/sale/order.php
@@ -1568,7 +1568,7 @@ class ControllerSaleOrder extends Controller {
 			$this->data['comment'] = nl2br($order_info['comment']);
 			$this->data['shipping_method'] = $order_info['shipping_method'];
 
-			$this->data['referrer_id'] = $this->model_sale_order->getReferrerId($order_id);
+			$this->data['referrer_id'] = $this->model_sale_order->getReferrerId((int)$order_id);
 
 
 			$this->data['payment_method'] = $order_info['payment_method'];
